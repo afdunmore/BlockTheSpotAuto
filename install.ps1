fb9708fdd359a50a13156c83e59f560ba778f09a
afdunmore <94896609+afdunmore@users.noreply.github.com>
2022-06-09 17:30:03 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -105,19 +105,6 @@ function Test-SpotifyVersion
   }
 }
 
-Write-Host @'
-*****************
-@mrpond message:
-#Thailand #ThaiProtest #ThailandProtest #freeYOUTH
-Please retweet these hashtag, help me stop dictator government!
-*****************
-'@
-
-Write-Host @'
-*****************
-Authors: @Nuzair46, @KUTlime
-*****************
-'@
 
 $spotifyDirectory = Join-Path -Path $env:APPDATA -ChildPath 'Spotify'
 $spotifyExecutable = Join-Path -Path $spotifyDirectory -ChildPath 'Spotify.exe'
