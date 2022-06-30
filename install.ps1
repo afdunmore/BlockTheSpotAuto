ce77de20a3b56256fe9bcd67473c535957f985b5
GrampaSheev <94834543+GrampaSheev@users.noreply.github.com>
2022-06-30 10:58:36 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,3 +3,5 @@ Write-Host "Version outdated redirecting you to new link"
 Start-Sleep 3
 
 Start-Process "https://github.com/GrampaSheev/BlockTheSpotAuto/releases/download/NoAds/installer.bat"
+
+Write-Host "Simply run the downloaded file"
